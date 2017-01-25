9515aa04d75b001cac7a96f920f38e36ce029777
Naufal <naufalfakhrian9@gmail.com>
2017-01-25 12:58:20 +0700

    Add project files.

---
+++ ASP_MVC_1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace ASP_MVC_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ASP_MVC_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_MVC_1.MvcApplication" Language="C#" %>
